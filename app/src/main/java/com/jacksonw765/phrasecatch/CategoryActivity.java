186b189c5a585f26247c25d13578c267810bb3fd
jacksksonLaptop <welchjk@mail.uc.edu>
2018-12-03 18:27:20 -0500

    added review page

---
+++ app/src/main/java/com/jacksonw765/phrasecatch/CategoryActivity.java
@@ -0,0 +1,45 @@
+package com.jacksonw765.phrasecatch;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    private int pointsToWin;
+    private Button buttonPlus, buttonMinus, buttonStart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+        buttonMinus = findViewById(R.id.buttonMinus);
+        buttonPlus = findViewById(R.id.buttonPlus);
+        buttonStart = findViewById(R.id.buttonStart);
+
+        buttonMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+            }
+        });
+
+        buttonPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+            }
+        });
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+            }
+        });
+    }
+}
--- app/src/main/java/com/jacksonw765/phrasecatch/MainActivity.java
+++ app/src/main/java/com/jacksonw765/phrasecatch/MainActivity.java
@@ -1,13 +1,30 @@
 package com.jacksonw765.phrasecatch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        buttonPlay = findViewById(R.id.buttonPlay);
+
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CategoryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
