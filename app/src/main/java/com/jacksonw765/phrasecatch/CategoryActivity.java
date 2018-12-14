d12facdeebffc71ed599c287ef089e34edea6783
welchjk <welchjk@mail.uc.edu>
2018-12-13 23:11:01 -0500

    added pointsToWin w/ UI working

--- app/src/main/java/com/jacksonw765/phrasecatch/CategoryActivity.java
+++ app/src/main/java/com/jacksonw765/phrasecatch/CategoryActivity.java
@@ -1,46 +1,70 @@
 package com.jacksonw765.phrasecatch;
 
-import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CategoryActivity extends AppCompatActivity {
 
-    private int pointsToWin;
     private Button buttonPlus, buttonMinus, buttonStart;
+    private int pointsToWin = 6;
+    private TextView textViewPointsToWin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_play);
+        setContentView(R.layout.activity_category);
         buttonMinus = findViewById(R.id.buttonMinus);
         buttonPlus = findViewById(R.id.buttonPlus);
         buttonStart = findViewById(R.id.buttonStart);
+        textViewPointsToWin = findViewById(R.id.textPointsToWin);
 
         buttonMinus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO
+                if(pointsToWin > 0) {
+                    try {
+                        final MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.type);
+                        mp.start();
+                        pointsToWin--;
+                        textViewPointsToWin.setText("" + pointsToWin);
+                    } catch (Exception e) {
+                        Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT);
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "Min score", Toast.LENGTH_SHORT);
+                }
             }
         });
 
         buttonPlus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final MediaPlayer mp = MediaPlayer.create(view.getContext(), R.raw.countdown_short);
-                
+                if (pointsToWin < 10) {
+                    try {
+                        final MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.type);
+                        mp.start();
+                        pointsToWin++;
+                        textViewPointsToWin.setText("" + pointsToWin);
+                    } catch (Exception e) {
+                        Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT);
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "Max score", Toast.LENGTH_SHORT);
+                }
             }
         });
 
         buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO
+                //TODO START GAME
             }
         });
     }
